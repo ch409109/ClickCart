ef70b4c4bae79b107a439df950f22a488f16453f
Hoang Thanh Cong <ch409109@gmail.com>
2025-01-09 15:36:24 +0700

    feat: add sql query

---
+++ DTB.sql
@@ -0,0 +1,85 @@
+﻿USE [master];
+GO
+
+-- Xóa database nếu đã tồn tại
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'ClickCart')
+BEGIN
+    DROP DATABASE ClickCart;
+END;
+GO
+
+-- Tạo lại database
+CREATE DATABASE ClickCart;
+GO
+
+-- Sử dụng database vừa tạo
+USE ClickCart;
+GO
+
+CREATE TABLE Roles (
+    RoleID INT PRIMARY KEY IDENTITY(1,1),
+    RoleName NVARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(100) NOT NULL,
+    FullName NVARCHAR(100),
+    Email NVARCHAR(100) UNIQUE,
+    Phone NVARCHAR(20),
+    Address NVARCHAR(200),
+    CreatedDate DATETIME DEFAULT GETDATE(),
+    Status BIT DEFAULT 1,
+    RoleID INT FOREIGN KEY REFERENCES Roles(RoleID)
+)
+
+CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryName NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(500),
+    Status BIT DEFAULT 1
+)
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY IDENTITY(1,1),
+    ProductName NVARCHAR(200) NOT NULL,
+    CategoryID INT FOREIGN KEY REFERENCES Categories(CategoryID),
+    Price INT NOT NULL,
+    Quantity INT DEFAULT 0,
+    Image NVARCHAR(200),
+    Status BIT DEFAULT 1
+)
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT FOREIGN KEY REFERENCES Users(UserID),
+    OrderDate DATETIME DEFAULT GETDATE(),
+    TotalAmount DECIMAL(18,2),
+    ShippingAddress NVARCHAR(200),
+    PhoneNumber NVARCHAR(20),
+    OrderStatus NVARCHAR(50) DEFAULT N'Chờ xác nhận',
+    PaymentStatus BIT DEFAULT 0
+)
+
+CREATE TABLE Cart (
+    CartID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT FOREIGN KEY REFERENCES Users(UserID),
+    CreatedDate DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE CartItems (
+    CartItemID INT PRIMARY KEY IDENTITY(1,1),
+    CartID INT FOREIGN KEY REFERENCES Cart(CartID),
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+    Quantity INT NOT NULL,
+    Price INT NOT NULL
+);
+
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY IDENTITY(1,1),
+    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+    Quantity INT NOT NULL,
+    Price INT NOT NULL
+)
